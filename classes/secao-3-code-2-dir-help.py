8cba084509e0f42930dde6f9f7ae7fe324dbc2ae
leonardo-delboni <leonardodelboni@gmail.com>
2021-08-06 17:07:12 -0300

    Daily Actualization

---
+++ classes/secao-3-code-2-dir-help.py
@@ -0,0 +1,15 @@
+#DIR e HELP são utilitários Python para auxiliar o programador durante a programação
+
+#Dir -> Apresenta todos os atributos e funções/métodos disponíveis para um determinado
+#tipo de dado ou variável
+
+#dir(tipo de dado/variável)
+#    pass
+
+#Help -> Apresenta a documentação de como utilizar atributos e funções/métodos disponíveis 
+#para um determinado tipo de dado ou variável
+
+#help(tipo de dado/variável.propriedade)
+#    pass
+
+import this
---
+++ classes/secao-3-recebendo-dados-do-usuario.py
@@ -0,0 +1,5 @@
+print("teste")
+a = 4
+b = 5
+c = a + b
+print(c)
